bc3ef7ea560962603befe52bd4f68356806523b6
MRTN <maarten@mrtn.be>
2025-06-26 11:39:22 +0200

    Version bump for testing workflow

--- mirae.php
+++ mirae.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Mirae
  * Plugin URI:        https://maartenkumpen.com
  * Description:       Mirae lets you build a customizable Linktree-style profile with platform buttons, icons, and custom text. All managed from your WordPress admin.
- * Version:           1.0.2
+ * Version:           1.0.3
  * Author:            Maarten Kumpen
  * Author URI:        https://maartenkumpen.com/
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'MIRAE_VERSION', '1.0.2' );
+define( 'MIRAE_VERSION', '1.0.3' );
 
 /**
  * The code that runs during plugin activation.
